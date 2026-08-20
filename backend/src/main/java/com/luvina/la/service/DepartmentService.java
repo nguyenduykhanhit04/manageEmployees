3052eafcf472997737f5800f1d4f330315254fdf
Nguyễn Duy Khánh <nguyenduykhanhit04@gmail.com>
2026-08-20 23:50:30 +0700

    Update lại Backend sau khi chỉnh sửa 1 số java docs

--- backend/src/main/java/com/luvina/la/service/DepartmentService.java
+++ backend/src/main/java/com/luvina/la/service/DepartmentService.java
@@ -2,20 +2,8 @@ package com.luvina.la.service;
 
 import com.luvina.la.payload.DepartmentResponse;
 
-/**
- * Interface định nghĩa các chức năng xử lý logic phòng ban.
- * Lớp này cung cấp các phương thức để lấy thông tin phòng ban từ database.
- *
- * @author nguyenduykhanh2
- * @since 1.0
- */
 public interface DepartmentService {
 
-    /**
-     * Lấy danh sách tất cả phòng ban.
-     *
-     * @return DepartmentResponse chứa mã trạng thái và danh sách DepartmentDTO
-     */
     DepartmentResponse getAllDepartments();
 }
 
